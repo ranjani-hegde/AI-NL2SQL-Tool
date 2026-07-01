c9264834dc8371752597bba89e266180316ef381
Ranjani Hegde <ranjanihegde2005@gmail.com>
2026-07-01 22:38:40 +0530

    Remove test files

--- schema_check.py
+++
@@ -1,5 +0,0 @@
-from langchain_community.utilities import SQLDatabase
-
-db = SQLDatabase.from_uri("sqlite:///sample.db")
-
-print(db.get_table_info())
--- test_ai.py
+++
@@ -1,4 +0,0 @@
-from ai_sql import generate_ai_sql
-
-sql = generate_ai_sql("Top selling product in January")
-print(sql)
